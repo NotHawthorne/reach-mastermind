8d65dbfd8225b65b9a538397a200b179252a7a3b
Alexander Kozma <alexanders.kozma@gmail.com>
2021-04-23 11:54:34 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const axios = require('axios');
+const { v4:uuid } = require('uuid');
+const _ = require('lodash');
+
+// array of objects that contain info on the current ongoing games
+var currentGames = {};
+
+// serve from public folder
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.render(index);
+});
+
+async function attempt(id, input, config) {
+	// start a new game if they haven't started one, they supplied an invalid id, or their config changed
+	if (id == "-1" || currentGames.hasOwnProperty(id) == false || _.isEqual(config, currentGames[id]['config']) != true) {
+		await axios({'method':'GET','url':'https://www.random.org/integers/?num=' + config.count + '&format=plain&min=' + config.minValue + '&max=' + config.maxValue + '&col=1&base=10&rnd=new'})
+		.then(await async function (response) {
+			var data = response.data.split('\n');
+			var nums = [];
+			var nextId = uuid().toString();
+			for (elem in data) {
+				if (isNaN(parseInt(data[elem])) != true)
+					nums.push(parseInt(data[elem]));
+			}
+
+			// create game session
+			currentGames[nextId] = {
+				'id': nextId,
+				'nums': nums,
+				'tries': 10,
+				'config': JSON.parse(JSON.stringify(config)),
+			}
+
+			// set the id to a valid one, so we can process the input
+			id = nextId;
+
+			console.log("[Mastermind] Creating new game of id " + id + " with nums " + nums);
+		});
+	}
+	console.log("[Mastermind] Processing attempt for game " + id);
+	console.log(input);
+	game = currentGames[id];
+	if (input === undefined || input.length > game['config']['count'] || input.length <= 0) {
+		console.log("[Mastermind] ERROR: Invalid input supplied");
+		return {'status': 'KO'}
+	}
+	else {
+		// compare numbers to correct answers
+		var matches = 0;
+		for (elem in input) {
+			if (input[elem] == game['nums'][elem])
+				matches++;
+		}
+		game['tries'] -= 1;
+
+		var retObj = {
+			'id': id,
+			'matches': matches,
+			'tries': game['tries'],
+			'status': 'OK',
+			'attemptedNumber': input,
+		};
+
+		// if we're out of tries, or we have guessed correctly, we are done with this game
+		if (matches == 4 || game['tries'] <= 0)
+			delete currentGames[id];
+		return game['tries'] > 0 ? retObj : null;
+	}
+}
+
+app.get('/attempt', async (req, res) => {
+	var id = req.query.id;
+	var input = req.query.input;
+	var ret = await attempt(id, input, {'count': req.query.count, 'minValue': req.query.minValue, 'maxValue': req.query.maxValue});
+	if (ret == null) {
+		res.send({'status': 'KO', 'attemptedNumber': input});
+		console.log("GAME OVER FOR GAME #"+id);
+	}
+	else {
+		console.log(ret);
+		res.send(ret);
+	}
+});
+
+app.listen(port, () => {
+	console.log("Listening...");
+});
---
+++ public/client.js
@@ -0,0 +1,155 @@
+// buttons
+const submitButton = document.getElementById('submitButton');
+const settingsButton = document.getElementById('settingsButton');
+const saveSettingsButton = document.getElementById('saveSettingsButton');
+
+// input forms
+const numbersForm = document.getElementById('numbers');
+const countForm = document.getElementById('count');
+const minValueForm = document.getElementById('minValue');
+const maxValueForm = document.getElementById('maxValue');
+
+// misc elements
+const historyDiv = document.getElementById('historyDiv');
+const numbersGroup = document.getElementById('numbersFormGroup');
+
+// globals
+var lock = false;
+var currentId = "-1";
+var reqLoc = new URL(window.location + "attempt"); // endpoint to process an attempt
+var maxHistory = 50;
+
+var gameConfig = {
+	'count': 4,
+	'minValue': 0,
+	'maxValue': 7,
+};
+
+function getCurrentNumbers() {
+	var forms = numbersGroup.getElementsByTagName('input');
+	var ret = [];
+	console.log(forms);
+	for (x in forms) {
+		if (isNaN(parseInt(forms[x].value)) != true)
+			ret.push(parseInt(forms[x].value));
+		else if (x < gameConfig['count'])
+			return null;
+	}
+	return ret;
+}
+
+function reloadGame() {
+	// clear the list
+	while (numbersGroup.firstChild)
+		numbersGroup.removeChild(numbersGroup.firstChild);
+	// populate based on config
+	for (var i = 0; i < gameConfig['count']; i++) {
+		numbersGroup.appendChild(document.createElement("br"));
+		var input = document.createElement("input");
+		input.setAttribute("type", "text");
+		input.setAttribute("id", "number" + i);
+		input.setAttribute("class", "form-control");
+		input.setAttribute("placeholder", "0");
+		numbersGroup.appendChild(input);
+	}
+}
+
+reloadGame();
+
+function addToHistory(str) {
+	// create history entry
+	var li = document.createElement('li');
+	var ul = historyDiv.getElementsByTagName('ul')[0];
+	li.appendChild(document.createTextNode(str));
+
+	// pop last element if we're over 20
+	if (ul.getElementsByTagName("li").length > maxHistory)
+		ul.removeChild(ul.lastChild);
+
+	// add to history at top
+	if (ul.firstChild) 
+		ul.insertBefore(li, ul.firstChild);
+	else
+		ul.appendChild(li);
+}
+
+async function submitNums() {
+	// lock our data while we're submitting
+	lock = true;
+	var attempt = getCurrentNumbers();
+
+	// we have backend checks for this, but let's not allow them to submit incomplete guesses
+	if (attempt == null) {
+		addToHistory("ERROR: Invalid attempt! Check your input.");
+		lock = false;
+		return ;
+	}
+
+	var params = new URLSearchParams(attempt.map(n=>['input',n]));
+	params.append('id', currentId);
+	params.append('count', gameConfig.count);
+	params.append('minValue', gameConfig.minValue);
+	params.append('maxValue', gameConfig.maxValue);
+	reqLoc.search = params;
+	await fetch(reqLoc).then(async function (response) {
+		var data = await response.json();
+		console.log(data);
+
+		// if we are on a different game, keep track of that
+		if (currentId != data.id) {
+			currentId = data.id;
+			addToHistory("NEW GAME! Guess " + gameConfig['count'] + " numbers!");
+		}
+		var resultString = "";
+
+		if (data['status'] == 'KO')
+			console.log("game over");
+		if (data['status'] == 'OK') {
+			resultString += "INPUT: " + data['attemptedNumber'] + " | RESULT: ";
+			switch (data['matches']) {
+				case 0:
+					resultString += "NONE";
+					break ;
+				case gameConfig['count']:
+					resultString += "SUCCESS";
+					break ;
+				default:
+					resultString += "PARTIAL (" + data['matches'] + ")";
+					break ;
+			}
+		}
+		else {
+			resultString += "FAIL";
+		}
+		addToHistory(resultString);
+		// ok we're done now, carry on
+		lock = false;
+	});
+}
+
+// listener for submissions
+submitButton.addEventListener('click', function(e) {
+	// prevent reload on button press
+	e.preventDefault();
+
+	// don't submit another request if we're still waiting on results
+	if (lock == false)
+		submitNums();
+	else
+		console.log("loading, please wait...");
+});
+
+// listener for config changes
+saveSettingsButton.addEventListener('click', function(e) {
+	// prevent reload on button press
+	e.preventDefault();
+
+	// if they have valid inputs for these fields, update their config entry
+	if (isNaN(parseInt(countForm.value)) != true)
+		gameConfig['count'] = parseInt(countForm.value);
+	if (isNaN(parseInt(minValueForm.value)) != true)
+		gameConfig['minValue'] = parseInt(minValueForm.value);
+	if (isNaN(parseInt(maxValueForm.value)) != true)
+		gameConfig['maxValue'] = parseInt(maxValueForm.value);
+	reloadGame();
+});
